4b46a77af1abed57ed8c97241cc975c10a4b9a4b
Patrick Horlebein <patrick@ilovepatrick.de>
2015-12-28 15:30:51 +0100

    Moved X11 dependency into GLX package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
                       name: "VistaX11",
                       dependencies: [
                           .Package(url: "https://github.com/Vista-swift/VistaCommon.git", majorVersion: 1),
-                          .Package(url: "https://github.com/PixelPirate/CX11.swift.git", majorVersion: 1),
+                          .Package(url: "https://github.com/Vista-swift/CGLX.git", majorVersion: 1),
                       ]
                       )
